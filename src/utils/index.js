35b5878783415de42825ec3dacbf89fbd8bd0068
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-15 12:16:48 -0300

    add hero

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,7 +3,13 @@ import yslHorizontal from '/assets/images/yslHorizontal.svg'
 import search from '/assets/images/search.svg'
 import bag from '/assets/images/bag.svg'
 
+import hmv from '/assets/videos/hero.mp4'
+import smallmv from '/assets/videos/smallHero.mp4'
+
 export const yslImg = ysl
 export const searchImg = search
 export const bagImg = bag
 export const yslHorizontalImg = yslHorizontal
+
+export const heroVideo = hmv
+export const smallHeroVideo = smallmv
