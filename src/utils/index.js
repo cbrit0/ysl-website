23543e6b96edf2a1ed8ec692973d1b61cc9eccad
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-15 16:55:12 -0300

    add images

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,11 @@ import bag from '/assets/images/bag.svg'
 import hmv from '/assets/videos/hero.mp4'
 import smallmv from '/assets/videos/smallHero.mp4'
 
+import y from '/assets/images/yEauDeParfum.png'
+import monParis from '/assets/images/monParisEauDeParfum.png'
+import blackOpium from '/assets/images/blackOpiumEauDeParfum.png'
+import myslf from '/assets/images/myslfEauDeParfum.png'
+
 export const yslImg = ysl
 export const searchImg = search
 export const bagImg = bag
@@ -13,3 +18,8 @@ export const yslHorizontalImg = yslHorizontal
 
 export const heroVideo = hmv
 export const smallHeroVideo = smallmv
+
+export const yImg = y
+export const monParisImg = monParis
+export const blackOpiumImg = blackOpium
+export const myslfImg = myslf
