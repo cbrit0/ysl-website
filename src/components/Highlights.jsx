05a4ab8026f80429004492342e5b992eaa80de0f
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-15 17:53:57 -0300

    add highlights

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,18 @@
+
+
+const Highlights = () => {
+  return (
+    <section 
+        id="highlights" 
+        className="w-screen overflow-hidden h-full common-padding bg-[url(/assets/images/highlightsBackground.webp)]"
+    >
+      <div className="screen-max-width">
+        <div className="mb-12 w-full md:flex items-end justify-center">
+          <h1 id="title" className="section-heading">OUR FRAGRANCE ICONS</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Highlights
