c513c0532b2bb6361458dc1867b6e5f302166bd9
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-22 19:48:42 -0300

    add shades images

--- src/utils/index.js
+++ src/utils/index.js
@@ -17,6 +17,29 @@ import secondHighlight from '/assets/videos/v-4dd63971-0016-4451-bb66-449b086881
 import thirdHighlight from '/assets/videos/v-337ae941-64bc-49af-92be-8d1cc946f66c.mp4'
 import fourthHighlight from '/assets/videos/v-5e07b294-9bb2-4376-9032-ec92f3056b04.mp4'
 
+import coralClashBg from '/assets/images/shades/3614274254297_mmbbb_shade-57_main.png'
+import coralClash from 'public/assets/images/shades/57_CORAL CLASH_web.jpg'
+import nudeLavalliereBg from 'public/assets/images/shades/3614274254310_mmbbb_shade-44_main.png'
+import nudeLavalliere from 'public/assets/images/shades/44_NUDE LAVALLIERE_web.jpg'
+import roseHazeBg from 'public/assets/images/shades/3614274254372_mmbbb_shade-06_main.png'
+import roseHaze from 'public/assets/images/shades/06_ROSE HAZE_web.jpg'
+import nocturnalNudeBg from 'public/assets/images/shades/3614274254358_mmbbb_shade-12_main.png'
+import nocturnalNude from 'public/assets/images/shades/12_NOCTURNAL NUDE_web.jpg'
+import suaveSunstoneBg from 'public/assets/images/shades/3614274254334_mmbbb_shade-24_main.png'
+import suaveSunstone from 'public/assets/images/shades/24_SUAVE SUNSTONE_web.jpg'
+import peachyNudeBg from 'public/assets/images/shades/3614274254334_mmbbb_shade-37_main.png'
+import peachyNude from 'public/assets/images/shades/37_PEACHY NUDE_web.jpg'
+import babydollPinkBg from 'public/assets/images/shades/3614274254334_mmbbb_shade-37_main.png'
+import babydollPink from 'public/assets/images/shades/42_BABYDOLL PINK_web.jpg'
+import berryBangBg from 'public/assets/images/shades/3614274254303_mmbbb_shade-54_main.png'
+import berryBang from 'public/assets/images/shades/54_BERRY BANG_web.jpg'
+import lavenderLustBg from 'public/assets/images/shades/3614274254327_mmbbb_shade-69_main.png'
+import lavenderLust from 'public/assets/images/shades/69_LAVENDER_LUST_web.jpg'
+import pinkVoltageBg from 'public/assets/images/shades/3614274254365_mmbbb_shade-87_main.png'
+import pinkVoltage from 'public/assets/images/shades/87_PINK VOLTAGE_web.jpg'
+import restlessRoseBg from 'public/assets/images/shades/3614274254341_mmbbb_shade-93_main.png'
+import restlessRose from 'public/assets/images/shades/93_RESTLESS ROSE_web.jpg'
+
 import play from '/assets/images/play.svg'
 import pause from '/assets/images/pause.svg'
 
@@ -41,3 +64,27 @@ export const fourthHighlightVideo = fourthHighlight
 
 export const playImg = play
 export const pauseImg = pause
+
+export const coralClashBgImg = coralClashBg
+export const coralClashImg = coralClash
+export const nudeLavalliereBgImg = nudeLavalliereBg
+export const nudeLavalliereImg = nudeLavalliere
+export const roseHazeBgImg = roseHazeBg
+export const roseHazeImg = roseHaze
+export const nocturnalNudeBgImg = nocturnalNudeBg
+export const nocturnalNudeImg = nocturnalNude
+export const suaveSunstoneBgImg = suaveSunstoneBg
+export const suaveSunstoneImg = suaveSunstone
+export const peachyNudeBgImg = peachyNudeBg
+export const peachyNudeImg = peachyNude
+export const babydollPinkBgImg = babydollPinkBg
+export const babydollPinkImg = babydollPink
+export const berryBangBgImg = berryBangBg
+export const berryBangImg = berryBang
+export const lavenderLustBgImg = lavenderLustBg
+export const lavenderLustImg = lavenderLust
+export const pinkVoltageBgImg = pinkVoltageBg
+export const pinkVoltageImg = pinkVoltage
+export const restlessRoseBgImg = restlessRoseBg
+export const restlessRoseImg = restlessRose
+
