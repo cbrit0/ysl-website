9f4b7ea037c8181fd864c5cf3198d93014d6857f
cbrit0 <cristobal.brito.p@gmail.com>
2025-02-27 10:59:11 -0300

    refactor quicklinks css class

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -4,7 +4,7 @@ const QuickLinks = () => {
   return (
     <section id="quicklinks" className="overflow-hidden py-5">
       <div className="screen-max-width flex-center">
-        <div className="w-[22%] flex-center border-gray-300 border rounded-full mr-4 hover:border-black hover:cursor-pointer">
+        <div className="w-[22%] quicklink-containter">
           <div className="mt-1 mb-1 flex-center">
             <img 
               src={bog1Img} 
@@ -14,19 +14,19 @@ const QuickLinks = () => {
             <small className="font-bold ml-2 text-sm">A JOURNEY INTO GLITTERING SENSUALITY</small>
           </div>
         </div>
-        <div className="w-[25%] flex-center border-gray-300 border rounded-full mr-4 hover:border-black hover:cursor-pointer">
+        <div className="w-[25%] quicklink-containter">
           <div className="mt-1 mb-1 flex-center">
             <img src={bog2Img} className="rounded-full ml-1"/>
             <small className="font-bold ml-2 text-sm">THE FRAGRANCE FINDER FOR EVERY MOOD</small>
           </div>
         </div>
-        <div className="w-[24%] flex-center border-gray-300 border rounded-full mr-4 hover:border-black hover:cursor-pointer">
+        <div className="w-[24%] quicklink-containter">
           <div className="mt-1 mb-1 flex-center">
             <img src={bog3Img} className="rounded-full ml-1"/>
             <small className="font-bold ml-2 text-sm mr-1">DISCOVER LOVESHINE LIPSTICK</small>
           </div>
         </div>
-        <div className="w-[25%] flex-center border-gray-300 border rounded-full mr-4 hover:border-black hover:cursor-pointer">
+        <div className="w-[25%] quicklink-containter">
           <div className="mt-1 mb-1 flex-center">
             <img src={bog4Img} className="rounded-full ml-1"/>
             <small className="font-bold ml-2 text-sm">YSL BEAUTY FRAGRANCES NOW IN A REFILLABLE DESIGN</small>
