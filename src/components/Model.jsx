f2436952a5e8f07dc12332a0208c5baf6964eae8
cbrit0 <cristobal.brito.p@gmail.com>
2025-03-07 12:47:45 -0300

    add responsiveness

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -27,37 +27,44 @@ const Model = () => {
   }, [])
 
   return (
-    <section id="model" className="common-padding bg-[url('/assets/images/hero-banner.jpg')] bg-cover bg-center bg-no-repeat">
-      <div className="flex justify-end">
-        <div className="w-[70vh] h-[75vh] md:h-[90vh] overflow-hidden relative border">
+    <section
+      id="model"
+      className="common-padding bg-[url('/assets/images/hero-banner.jpg')] bg-cover bg-center bg-no-repeat"
+    >
+      <div className="flex flex-col md:flex-row justify-end">
+        <div className="w-full md:w-1/2 h-auto md:h-[90vh] p-4 md:p-45 flex-center order-1 md:order-2">
+          <div className="flex-col md:text-left text-center">
+            <h1 id="heading" className="text-3xl md:text-4xl font-bold mb-4">
+              New Collection. <br /> Timeless Style.
+            </h1>
+            <p id="body-text" className="mb-4">
+              Step into the future of luxury with the Opyum Sandals—crafted in
+              patent leather, designed for the modern icon.
+            </p>
+
+            <div className="flex md:justify-start justify-center">
+              <a className="discover-now-button w-45">DISCOVER</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-full md:w-[70vh] h-[50vh] md:h-[90vh] overflow-hidden relative order-2 md:order-1">
           <ModelView />
 
           <Canvas
             className="w-full h-full"
             style={{
-              position: 'fixed',
+              position: "fixed",
               top: 0,
               bottom: 0,
               left: 0,
               right: 0,
-              overflow: 'hidden'
+              overflow: "hidden",
             }}
-            eventSource={document.getElementById('root')}
+            eventSource={document.getElementById("root")}
           >
             <View.Port />
           </Canvas>
-        </div>
-
-        <div className="w-1/2 h-[75vh] md:h-[90vh] p-45 flex-center">
-          <div className="flex-col">
-
-            <h1 id="heading" className="text-4xl font-bold mb-4">New Collection. <br/> Timeless Style.</h1>
-            <p id="body-text" className="mb-4">
-              Step into the future of luxury with the Opyum Sandals—crafted in patent leather, designed for the modern icon.
-            </p>
-
-            <a className="discover-now-button w-40">DISCOVER</a>
-          </div>
         </div>
       </div>
     </section>
