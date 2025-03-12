7913837b080ea35aaa3f52bf4028eefecb374b34
cbrit0 <cristobal.brito.p@gmail.com>
2025-03-12 17:32:56 -0300

    change to swiper

--- src/components/QuickLinks.jsx
+++ src/components/QuickLinks.jsx
@@ -1,51 +1,45 @@
-import { Splide, SplideSlide } from "@splidejs/react-splide"
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, FreeMode} from 'swiper/modules'
 import { bog1Img, bog2Img, bog3Img, bog4Img } from "../utils"
-
-const splideOptions = {
-  perPage: 4,
-  perMove: 4,
-  pagination: false,
-  arrows: false,
-}
+import 'swiper/css'
+import 'swiper/css/navigation'
+import '../styles.css'
 
 const QuickLinks = () => {
   return (
-    <section id="quicklinks" className="overflow-hidden py-5">
+    <section id="quicklinks" className="overflow-hidden border">
       <div className="screen-max-width flex-center">
-        <Splide options={splideOptions}>
-          <SplideSlide className="lg:flex-center">
-          <div className="w-[80%] quicklink-containter lg:ml-12">
-            <div className="quicklink-text-container">
-              <img src={bog1Img} className="rounded-full ml-1" alt="bog1"/>
-              <small className="quicklink-text">A JOURNEY INTO GLITTERING SENSUALITY</small>
+        <Swiper
+          modules={[Navigation, FreeMode]}
+          freeMode={true}
+          slidesPerView={'auto'}
+          spaceBetween={80}
+        >
+          <SwiperSlide style={{ width: 'auto' }}>
+            <div className="quicklink-containter">
+              <img src={bog1Img} className="rounded-full" alt="bog1"/>
+              <small className="quicklink-text">A JOURNEY INTO <br/> GLITTERING SENSUALITY</small>
             </div>
-          </div>
-          </SplideSlide>
-          <SplideSlide className="lg:flex-center">
-            <div className="w-[90%] quicklink-containter">
-              <div className="quicklink-text-container">
-                <img src={bog2Img} className="rounded-full ml-1"/>
-                <small className="quicklink-text">THE FRAGRANCE FINDER FOR EVERY MOOD</small>
-              </div>
+          </SwiperSlide>
+          <SwiperSlide style={{ width: 'auto' }}>
+            <div className="quicklink-containter">
+              <img src={bog2Img} className="rounded-full"/>
+              <small className="quicklink-text">THE FRAGRANCE FINDER <br/> FOR EVERY MOOD</small>
             </div>
-          </SplideSlide>
-          <SplideSlide className="lg:flex-center">
-            <div className="w-[96%] quicklink-containter">
-              <div className="quicklink-text-container">
-                <img src={bog3Img} className="rounded-full"/>
-                <small className="quicklink-text mr-1">DISCOVER LOVESHINE LIPSTICK</small>
-              </div>
+          </SwiperSlide>
+          <SwiperSlide style={{ width: 'auto' }}>
+            <div className="quicklink-containter">
+              <img src={bog3Img} className="rounded-full"/>
+              <small className="quicklink-text">DISCOVER LOVESHINE LIPSTICK</small>
             </div>
-          </SplideSlide>
-          <SplideSlide className="lg:flex-center">
-            <div className="w-[97%] quicklink-containter ml-3">
-              <div className="quicklink-text-container">
-                <img src={bog4Img} className="rounded-full ml-1"/>
-                <small className="quicklink-text">YSL BEAUTY FRAGRANCES NOW IN A REFILLABLE DESIGN</small>
-              </div>
+          </SwiperSlide>
+          <SwiperSlide style={{ width: 'auto' }}>
+            <div className="quicklink-containter">
+              <img src={bog4Img} className="rounded-full"/>
+              <small className="quicklink-text">YSL BEAUTY FRAGRANCES NOW <br/> IN A REFILLABLE DESIGN</small>
             </div>
-          </SplideSlide>
-        </Splide>
+          </SwiperSlide>
+        </Swiper> 
       </div>
     </section>
   )
