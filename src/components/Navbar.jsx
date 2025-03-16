03a87ff5e4edf7e8515c415104c49957fab8d1b7
cbrit0 <cristobal.brito.p@gmail.com>
2025-03-16 17:30:32 -0300

    fix navbar animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import { useEffect, useRef, useState } from "react"
 import { gsap } from "gsap"
 import { yslHorizontalWhiteImg, yslHorizontalBlackImg } from "../utils"
 import { navList } from '../constants'
+import { useGSAP } from "@gsap/react"
+
+gsap.registerPlugin(useGSAP)
 
 const Navbar = () => {
   const [isHovered, setIsHovered] = useState(false)
   const [hoveredIndex, setHoveredIndex] = useState(null)
   const [isSticky, setIsSticky] = useState(false)
   const [isAtTop, setIsAtTop] = useState(true)
-  const headerRef = useRef(null)
   const lastScrollY = useRef(0)
 
   const handleMouseEnter = () => {
@@ -50,66 +52,77 @@ const Navbar = () => {
     }
   }, [])
 
-  useEffect(() => {
-    const headerElement = headerRef.current
-
+  useGSAP(() => {
     if (isSticky && !isAtTop) {
       gsap.fromTo(
-        headerElement,
-        { top: '-100%' },
+        '#sticky-navbar',
+        { y: '-99%' },
         {
-          top: 0,
+          y: 0,
           position: 'fixed',
-          duration: 0.3,
+          duration: 0.2,
           ease: 'power2.out'
         }
       )
     } else {
-      gsap.to(headerElement, {
-        position: 'absolute',
-        top: 0,
-        duration: 0.2,
-        ease: 'power2.out'
-      })
+      gsap.fromTo(
+        '#sticky-navbar',
+        { top: 0 },
+        {
+          y: '-99%',
+          position: 'fixed',
+          duration: 0.2,
+          ease: 'power2.out'
+        }
+      )
     }
   }, [isSticky, isAtTop])
 
   return (
-    <header 
-      ref={headerRef}
-      className={`absolute top-0 left-0 w-full pt-5 sm:px-10 px-5 z-20 group ${
-        isSticky || isHovered ? 'bg-white' : 'bg-transparent'
+    <header
+      id="header"
+      className={`absolute top-0 left-0 w-full pt-5 px-5 z-20 ${
+        isHovered ? 'bg-white' : 'bg-transparent'
       } transition-colors duration-300 ease-in`}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {isAtTop && (
-        <div className="flex justify-center">
-          <img 
-            src={isHovered ? yslHorizontalBlackImg : yslHorizontalWhiteImg} 
-            alt="YSL"
-            className="cursor-pointer mb-10"
-          />
-        </div>
-      )}
+      <div className="flex-center mb-6">
+        <img 
+          src={isHovered ? yslHorizontalBlackImg : yslHorizontalWhiteImg} 
+          alt="YSL"
+          className="cursor-pointer"
+        />
+      </div>
 
-      <nav className="flex justify-center">
-        <div className="flex flex-wrap justify-center gap-2">
-          {navList.map((nav, index) => (
-            <p
-              key={index}
-              className={`text-[11px] font-bold text-sm cursor-pointer pb-4 pl-4 pr-4 ${
-                isSticky || isHovered ? 'text-black' : 'text-white'
-              } ${
-                hoveredIndex === index && 'border-b-3'
-              }`}
-              onMouseEnter={() => setHoveredIndex(index)}
-              onMouseLeave={() => setHoveredIndex(null)}
-            >
+      <nav id="navbar" className="flex-center gap-2">
+        {navList.map((nav, index) => (
+          <div
+            key={index}
+            className={`p-4 cursor-pointer ${hoveredIndex === index ? 'border-b-3' : ''}`}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <p className={`text-[11px] font-bold text-sm ${isHovered ? 'text-black' : 'text-white'}`}>
               {nav}
             </p>
-          ))}
-        </div>
+          </div>
+        ))}
+      </nav>
+
+      <nav id="sticky-navbar" className="absolute top-0 left-0 w-full z-30 flex-center gap-12 bg-white">
+        {navList.map((nav, index) => (
+          <div
+            key={index}
+            className={`p-4 cursor-pointer ${hoveredIndex === index ? 'border-b-3' : ''}`}
+            onMouseEnter={() => setHoveredIndex(index)}
+            onMouseLeave={() => setHoveredIndex(null)}
+          >
+            <p className="text-[11px] font-bold text-sm">
+              {nav}
+            </p>
+          </div>
+        ))}
       </nav>
     </header>
   )
