76f8531beeb71e92237a6217ca42192c939a3d6a
cbrit0 <cristobal.brito.p@gmail.com>
2025-03-20 22:39:13 -0300

    minor animation fix

--- src/components/MiniNavbar.jsx
+++ src/components/MiniNavbar.jsx
@@ -9,22 +9,29 @@ const MiniNavbar = ({ isMenuOpen, toggleMenu }) => {
   return (
     <header
       className={`absolute top-0 left-0 w-full p-4 z-20 ${
-        isHovered ? 'bg-white' : 'bg-transparent'
+        isHovered || isMenuOpen ? 'bg-white' : 'bg-transparent'
       } transition-colors duration-300 ease-in`}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <div className="flex items-center justify-between">
         <img 
-          src={isHovered ? yslHorizontalBlackImg : yslHorizontalWhiteImg} 
+          src={isHovered || isMenuOpen ? yslHorizontalBlackImg : yslHorizontalWhiteImg} 
           alt="YSL"
           className="w-26"
         />
 
         <div>
-          <FontAwesomeIcon icon={faMagnifyingGlass} className={`mr-8 ${isHovered ? 'text-black' : 'text-white'}`} />
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className={`mr-8 ${isHovered || isMenuOpen ? 'text-black' : 'text-white'}`} 
+          />
 
-          <FontAwesomeIcon icon={isMenuOpen ? faXmark : faBars} className={`${isHovered ? 'text-black' : 'text-white'}`} onClick={toggleMenu} />
+          <FontAwesomeIcon 
+            icon={isMenuOpen ? faXmark : faBars} 
+            className={`${isHovered || isMenuOpen ? 'text-black' : 'text-white'}`} 
+            onClick={toggleMenu} 
+          />
         </div>
       </div>
     </header>
