6f7705c1333738b0c08d54f13803072e526c5cb0
cbrit0 <cristobal.brito.p@gmail.com>
2025-03-23 22:39:04 -0300

    add cruelty free moving banner

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,9 @@
-import { yslBeautyClubImg, smallYslBeautyClubImg } from "../utils"
+import { yslBeautyClubImg, smallYslBeautyClubImg, crueltyFreeImg } from "../utils"
 import { useState, useEffect } from "react"
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
+gsap.registerPlugin(useGSAP)
 
 const Experience = () => {
   const [imageSrc, setImageSrc] = useState(window.innerWidth < 760 ? smallYslBeautyClubImg : yslBeautyClubImg)
@@ -20,12 +24,30 @@ const Experience = () => {
     }
   }, [])
 
+  useGSAP(() => {
+    gsap.to(
+      '#cruelty-free', {
+        backgroundPosition: '-100% 0',
+        duration: 20,
+        ease: 'linear',
+        repeat: -1
+      }
+
+    )
+  }, [])
+
   return (
     <section className="relative w-full h-screen overflow-hidden bg-black">
       <img 
         className="w-full h-auto object-cover" 
         src={imageSrc} 
         alt="YSL Beauty Club" 
+      />
+      
+      <span 
+        id="cruelty-free"
+        className="block h-10 bg-repeat bg-[length:auto_100%]"
+        style={{ backgroundImage: `url(${crueltyFreeImg})` }}
       />
     </section>
   )
